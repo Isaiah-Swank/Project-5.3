5c672f5c25da212db1068b4cf1dc3725677e4809
Nile Roth <102401663+nile-roth@users.noreply.github.com>
2024-04-22 15:58:56 -0700

    Update main.c

--- server/main.c
+++ server/main.c
@@ -8,7 +8,7 @@ ChatNodeList* chat_nodes;
  * MAIN
  ************************************************************************/
 
-int main(int argc, char** argv) {
+int main(int argc, char* argv[]) {
     
     int server_socket;                 // descriptor of server socket
     struct sockaddr_in server_address; // for naming the server's listening socket
@@ -36,8 +36,8 @@ int main(int argc, char** argv) {
     // name the socket (making sure the correct network byte ordering is observed)
     server_address.sin_family      = AF_INET;           // accept IP addresses
     server_address.sin_addr.s_addr = htonl(INADDR_ANY); // accept clients on any interface
-    server_address.sin_port        = htons(PORT);       // port to listen on
-    
+    server_address.sin_port        = htons(atoi(property_get_property( properties, "SERVER_PORT" )));       
+
     // binding unnamed socket to a particular port
     if (bind(server_socket, (struct sockaddr *)&server_address, sizeof(server_address)) != 0) {
         perror("Error binding socket");
