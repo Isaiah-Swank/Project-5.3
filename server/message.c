ca510838a69000536c42960818a04fb547ea71c9
Nile Roth <102401663+nile-roth@users.noreply.github.com>
2024-04-22 15:14:26 -0700

    Update message.c

--- server/message.c
+++ server/message.c
@@ -6,114 +6,66 @@ Message* new_message(int type, ChatNode* chat_node_ptr, char* note)
 {
     Message* new_message = malloc(sizeof(Message));
 
-    // switch statement checking for Individual or Group Messages
-    switch(type) {
-
-        // Individual Client JOIN Message
-        case JOIN:
-
-            // set the new message to the indivdual JOIN message
-            new_message->type = type;
-            new_message->chat_node = *chat_node_ptr;
-            strcpy(new_message->note, note);
-
-            break;
-            
-        // Group message "*** is Joining the Chat"
-        case JOINING:
-
-            // set the new message to the group JOINING message
-            new_message->type = type;
-            new_message->chat_node = *chat_node_ptr;
-            strcpy(new_message->note, note);
-
-            break;
-        // Individual Client LEAVE Message
-        case LEAVE:
-
-            // set the new message to the indivdual LEAVE message
-            new_message->type = type;
-            new_message->chat_node = *chat_node_ptr;
-            strcpy(new_message->note, note);
-
-            break;
-        // Group message "*** left the Chat"
-        case LEFT:
- 
-            // set the new message to the group LEFT message
-            new_message->type = type;
-            new_message->chat_node = *chat_node_ptr;
-            strcpy(new_message->note, note);
-
-            break;
-
-        // Normal text 'note' being sent
-        case NOTE:
-
-            new_message->type = type;
-            new_message->chat_node = *chat_node_ptr;
-            strcpy(new_message->note, note);
-            break;
-
-        // shutdown message to individual client
-        case SHUTDOWN:
-
-            new_message->type = type;
-            new_message->chat_node = *chat_node_ptr;
-            strcpy(new_message->note, note);
-            break;
-
-        // shutdown message to ALL clients
-        case SHUTDOWN_ALL:
-
-            new_message->type = type;
-            new_message->chat_node = *chat_node_ptr;
-            strcpy(new_message->note, note);
-            break;
-
-        // default, not a valid command
-        default:
-            
-            // send error message
-            printf("ERROR IN CREATING NEW MESSAGE\n");
-            exit(EXIT_FAILURE);
-            break;
-    }
+    // set the new message to the indivdual JOIN message
+    new_message->type = type;
+    new_message->chat_node = *chat_node_ptr;
+    strncpy(new_message->note, note, sizeof(new_message->note) -1);
 
     // return the new message
     return new_message;
 }
-size_t send_message(int socket, Message* message_ptr)
+ssize_t send_message(int socket, Message* message_ptr)
 {
 
-    size_t bytes_sent;
+    ssize_t bytes_sent;
     
     // send the message to the IP in the socket
     bytes_sent = send(socket, message_ptr, sizeof(Message), 0);
 
+    // check if there was an error sending the message
+    if(bytes_sent == -1)
+    {
+        perror("Error sending message");
+    }
+
     // return the number of bytes sent
     return bytes_sent;
 }
-size_t recieve_message(int socket, Message* message_ptr)
+ssize_t recieve_message(int socket, Message* message_ptr)
 {
     int index = 0;
     size_t bytes_read;
 
     // iterate through the message
-    while( message_ptr->note[index - 1] != '\0' )
+    while( index < sizeof(message_ptr->note) - 1)
     {
-        // recieve the message from the IP in the socket
-        bytes_read = read(socket, message_ptr->note, strlen(message_ptr->note));
+        // read the current character
+        bytes_read = read(socket, message_ptr->note + index, sizeof(message_ptr->note) - index - 1);
+
+        // check if error reading current byte
+        if(bytes_read <= 0)
+        {
+            // print error message
+            perror("Error reading message");
+            return bytes_read;
+        }
 
         // increment the index
         index += bytes_read;
-    }
-        
-    // print the recieved message
-    printf("%s\n", message_ptr->note);
 
-    // return number of bytes read
-    return bytes_read;
+        // check if at the end of the message
+        if(message_ptr->note[index - 1] == '\0')
+        {
+            // break off since the message is recieved
+            break; 
+        }
+
+        // print the new message
+        printf("%s\n", message_ptr->note);
+
+        // return the index
+        return index;
+    }
 }
 
 int main()
