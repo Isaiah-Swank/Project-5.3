19a761a634da9dbe463c559b648f8ee056507250
Ethan May <157064373+ethanmay476@users.noreply.github.com>
2024-04-22 16:20:41 -0700

    Update sender_handler.h

--- client/sender_handler.h
+++ client/sender_handler.h
@@ -3,8 +3,14 @@
 
 
 #include "properties.h" 
-#include "message.h"    
-#include "main.h" 
+#include "message.h" 
+#include <stdio.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+
 typedef enum {
     REQUEST_JOIN,
     REQUEST_LEAVE,
@@ -17,6 +23,15 @@ typedef enum {
 
 
 void *send_to_server(void *arg);
+int setup_client_socket(const char *ip, int port);
+void send_join_request(int socket, const char* message);
+void send_leave_request(int socket);
+void send_shutdown_request(int socket);
+void send_shutdown_all_request(int socket);
+void send_note(int socket, const char* message);
+
+
+
 
 #endif 
 
