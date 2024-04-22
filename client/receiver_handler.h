eb6ccd676bae94275f60dd0e9e773586c37ad96e
Isaiah-Swank <144286398+Isaiah-Swank@users.noreply.github.com>
2024-04-22 14:53:08 -0700

    Update receiver_handler.h

--- client/receiver_handler.h
+++ client/receiver_handler.h
@@ -1,10 +1,9 @@
 #ifndef RECEIVER_HANDLER_H
 #define RECEIVER_HANDLER_H
 
-#include "properties.h"  // Assuming properties are used within the receiver handler
-#include "message.h"     // Assuming a common message structure is defined here
+#include "properties.h" 
+#include "message.h" 
 
-// Function prototype for the thread that handles receiving messages from the server
 void *receive_from_server(void *arg);
 
 #endif // RECEIVER_HANDLER_H
