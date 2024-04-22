fd82908eee774e00db549cf3bb5367b89244f407
Isaiah-Swank <144286398+Isaiah-Swank@users.noreply.github.com>
2024-04-22 14:54:21 -0700

    Update receiver_handler.h

--- client/receiver_handler.h
+++ client/receiver_handler.h
@@ -1,9 +1,1 @@
-#ifndef RECEIVER_HANDLER_H
-#define RECEIVER_HANDLER_H
 
-#include "properties.h"  
-#include "message.h"     
-
-void *receive_from_server(void *arg);
-
-#endif // RECEIVER_HANDLER_H
