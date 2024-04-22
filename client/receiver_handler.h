0981cbee965cf30a8e9622b71ed57f1b157c127a
Ethan May <157064373+ethanmay476@users.noreply.github.com>
2024-04-21 22:06:32 -0700

    Update receiver_handler.h

--- client/receiver_handler.h
+++ client/receiver_handler.h
@@ -1,10 +1,10 @@
-#define RECEIVER_HANDLER_H
 #ifndef RECEIVER_HANDLER_H
+#define RECEIVER_HANDLER_H
 
+#include "properties.h"  // Assuming properties are used within the receiver handler
+#include "message.h"     // Assuming a common message structure is defined here
 
-#include "main.h" // For Client struct
-
-// The function executed by the receiver thread
-void receive_messages();
+// Function prototype for the thread that handles receiving messages from the server
+void *receive_from_server(void *arg);
 
 #endif // RECEIVER_HANDLER_H
