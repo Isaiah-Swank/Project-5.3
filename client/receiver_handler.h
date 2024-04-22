55f515b7d06365538e8e43c508c026a9bc5e0485
Isaiah-Swank <144286398+Isaiah-Swank@users.noreply.github.com>
2024-04-22 16:53:25 -0700

    Update receiver_handler.h

--- client/receiver_handler.h
+++ client/receiver_handler.h
@@ -3,7 +3,14 @@
 
 #include "properties.h" 
 #include "message.h" 
+#include <stdio.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+
 
 void *receive_from_server(void *arg);
-
+int setup_client_socket(const char *ip, int port);
 #endif // RECEIVER_HANDLER_H
