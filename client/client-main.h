7c07a7d4ae70af28245eb06ac99cdb82f2d90ad0
Nile Roth <102401663+nile-roth@users.noreply.github.com>
2024-04-22 21:16:17 -0700

    Update client-main.h

--- client/client-main.h
+++ client/client-main.h
@@ -4,6 +4,9 @@
 
 #include "chat_node.h"
 #include "message.h"
+#include "properties.h"
+#include "sender_handler.h"
+#include "receiver_handler.h"
 #include <pthread.h>
 
 int main(int argc, char* argv[]);
