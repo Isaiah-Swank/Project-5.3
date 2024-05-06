58cabfe5757f2979cb19ef6937fbe43dff807047
Ethan May <157064373+ethanmay476@users.noreply.github.com>
2024-05-05 23:26:08 -0700

    Update client_handler.h

--- server/client_handler.h
+++ server/client_handler.h
@@ -3,15 +3,12 @@
 #ifndef CLIENT_HANDLER_H
 #define CLIENT_HANDLER_H
 
-/************************************************************************
- * libraries
- ************************************************************************/
-// should always be there ...
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
 
-// socket/bind/listen/accept
+// Socket/bind/listen/accept
 #include <arpa/inet.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
@@ -19,7 +16,7 @@
 #include <signal.h>
 #include <pthread.h>
 
-// read/write/close
+// Read/write/close
 #include <sys/uio.h>
 #include <sys/types.h>
 #include <unistd.h>
@@ -27,10 +24,14 @@
 #include "message.h"
 #include "chat_node.h"
 
-// Function prototypes
+
+
+void add_client(int client_sock);
+void remove_client(int client_sock);
+void broadcast_message(const Message *msg, int sender_sock);
 void *talk_to_client(void *socket);
 
-//allows use of mutex from other c files
+// Allows use of mutex from other c files
 extern pthread_mutex_t mutex_client_socket;
 extern pthread_mutex_t mutex_chat_node_list;
 extern ChatNodeList* chat_nodes;
