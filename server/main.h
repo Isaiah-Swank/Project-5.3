5fec8055031c198e74de668676acd35bb85d726d
Ethan May <157064373+ethanmay476@users.noreply.github.com>
2024-05-05 22:47:21 -0700

    Update main.h

--- server/main.h
+++ server/main.h
@@ -1,9 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/************************************************************************
- * libraries
- ************************************************************************/
+
 // should always be there ...
 #include <stdio.h>
 #include <stdlib.h>
@@ -25,18 +23,15 @@
 #include "chat_node.h"
 #include "properties.h"
 #include "client_handler.h"
-/************************************************************************
- * preprocessor directives
- ************************************************************************/
-#define STANDARD_SIZE 128
-#define SERVER_ADDR "127.0.0.1" // loopback ip address
-#define PORT 23657              // port the server will listen on
 
-// I cannot let go of the old-fashioned way :) - for readability ...
+#define STANDARD_SIZE 128
+#define SERVER_ADDR "127.0.0.1" // ip address
+#define PORT 23657              // server port
+
 #define FALSE false
 #define TRUE !false
 
-// number of pending connections in the connection queue
+// number of connectsions
 #define NUM_CONNECTIONS 1
 
 #endif
